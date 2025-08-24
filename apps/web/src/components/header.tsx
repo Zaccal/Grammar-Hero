6725de89b393847e03029120a1753a06bb9f65ab
Adlet <ksss90411@gmail.com>
2025-08-24 20:07:19 +0500

    main ✨ init

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,31 +1,27 @@
 import { Link } from "@tanstack/react-router";
-import { ModeToggle } from "./mode-toggle";
-import UserMenu from "./user-menu";
+import { Code, Github } from "lucide-react";
 
 export default function Header() {
-	const links = [
-		{ to: "/", label: "Home" },
-		{ to: "/dashboard", label: "Dashboard" },
-	];
-
-	return (
-		<div>
-			<div className="flex flex-row items-center justify-between px-2 py-1">
-				<nav className="flex gap-4 text-lg">
-					{links.map(({ to, label }) => {
-						return (
-							<Link key={to} to={to}>
-								{label}
-							</Link>
-						);
-					})}
-				</nav>
-				<div className="flex items-center gap-2">
-					<ModeToggle />
-					<UserMenu />
-				</div>
-			</div>
-			<hr />
-		</div>
-	);
+  return (
+    <header className="py-4">
+      <nav className="container flex items-center justify-between sm:justify-around mx-auto px-4 sm:px-6 lg:px-8">
+        <Link to="/" className="text-xl font-black flex items-center">
+          <img src="/Logo-light.png" alt="Logo" className="w-20" />
+          <span>Grammar Hero</span>
+        </Link>
+        <ul className="flex items-center gap-8 px-1">
+          <li title="My GitHub profile">
+            <a target="_blank" href="https://github.com/Zaccal">
+              <Github />
+            </a>
+          </li>
+          <li title="Source Code">
+            <a target="_blank" href="https://github.com/Zaccal">
+              <Code />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
