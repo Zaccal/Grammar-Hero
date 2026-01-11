2582ec62041dd21a3a72f7659b807baabc60a428
Zaccal <ksss90411@gmail.com>
2026-01-11 17:09:18 +0500

    dev ✨ [feat]: made get provider endpoint

--- apps/server/src/routers/account/account.controller.ts
+++ apps/server/src/routers/account/account.controller.ts
@@ -1,5 +1,7 @@
 import type { Context } from '@/lib/context'
 import type { SetPasswordSchema } from '@/schemas/setPassword.schema'
+import { TRPCError } from '@trpc/server'
+import prisma from 'prisma'
 import { auth } from '@/lib/auth'
 
 export async function setPassword(ctx: Context, data: SetPasswordSchema) {
@@ -7,4 +9,24 @@ export async function setPassword(ctx: Context, data: SetPasswordSchema) {
     body: data,
     headers: ctx.headers,
   })
+}
+
+export async function getProvider(userId: string) {
+  // prettier-ignore
+  try {
+    const account = await prisma.account.findFirst({
+      where: {
+        userId,
+      },
+    })
+
+    return account?.providerId || null
+  }
+  catch (error) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Failed to get provider',
+      cause: error
+    })
+  }
 }
--- apps/server/src/routers/account/account.route.ts
+++ apps/server/src/routers/account/account.route.ts
@@ -1,9 +1,12 @@
 import { protectedProcedure, router } from '@/lib/trpc'
 import { setPasswordSchema } from '@/schemas/setPassword.schema'
-import { setPassword } from './account.controller'
+import { getProvider, setPassword } from './account.controller'
 
 export const accountRouter = router({
   changePassword: protectedProcedure
     .input(setPasswordSchema)
     .query(({ ctx, input }) => setPassword(ctx, input)),
+  getProvider: protectedProcedure.query(({ ctx }) =>
+    getProvider(ctx.session.user.id)
+  ),
 })
