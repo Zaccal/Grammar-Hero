216ed92d484d9c88d63dfe1e89abde75ded57c49
Zaccal <ksss90411@gmail.com>
2026-01-22 20:31:55 +0500

    dev ✨ [fix]: fixed building error

--- apps/web/src/hooks/useListAccounts.ts
+++ apps/web/src/hooks/useListAccounts.ts
@@ -4,12 +4,14 @@ import { authClient } from '@/lib/auth-client'
 export const USE_LIST_ACCOUNTS_QUERY_KEY = ['useListAccount']
 
 export function useListAccounts() {
- return useQuery({
-  queryKey: USE_LIST_ACCOUNTS_QUERY_KEY,
-  queryFn: async () => {
-   const response = await authClient.listAccounts()
-   if (response.error) { throw response.error }
-   return response.data
-  }
- })
+  return useQuery({
+    queryKey: USE_LIST_ACCOUNTS_QUERY_KEY,
+    queryFn: async () => {
+      const response = await authClient.listAccounts()
+      if (response.error) {
+        throw response.error
+      }
+      return response.data
+    },
+  })
 }
