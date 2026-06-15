6de841b0a304765ad53d8865bde0e3daf272829c
Zaccal <ksss90411@gmail.com>
2026-06-16 01:11:00 +0500

    main ✨ [fix]: replace S3 client from bun to lib

--- apps/server/src/lib/r2Client.ts
+++ apps/server/src/lib/r2Client.ts
@@ -1,9 +1,43 @@
+import { Buffer } from 'node:buffer'
 import process from 'node:process'
-import { S3Client } from 'bun'
+import {
+  DeleteObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3'
 
-export const r2Client = new S3Client({
+interface WriteOptions {
+  type?: string
+}
+
+const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY ?? '',
+    secretAccessKey: process.env.R2_SECRET_KEY ?? '',
+  },
   endpoint: process.env.R2_ENDPOINT,
-  bucket: process.env.R2_BUCKET,
-  accessKeyId: process.env.R2_ACCESS_KEY,
-  secretAccessKey: process.env.R2_SECRET_KEY,
+  forcePathStyle: true,
+  region: process.env.R2_REGION ?? 'auto',
 })
+
+export const r2Client = {
+  async write(key: string, file: File, options?: WriteOptions) {
+    await s3Client.send(
+      new PutObjectCommand({
+        Body: Buffer.from(await file.arrayBuffer()),
+        Bucket: process.env.R2_BUCKET,
+        ContentType: options?.type || file.type || 'application/octet-stream',
+        Key: key,
+      })
+    )
+  },
+
+  async delete(key: string) {
+    await s3Client.send(
+      new DeleteObjectCommand({
+        Bucket: process.env.R2_BUCKET,
+        Key: key,
+      })
+    )
+  },
+}
